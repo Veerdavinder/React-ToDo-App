30479bafb1570ea8a1c8f2004dc1aa7b2cc5117c
Birdevinder Singh <Veer davinder>
2019-10-29 20:04:51 -0400

    aligning server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ let Todo = require('./todo.model')
 app.use(cors());
 app.use(bodyParse.json());
 
-mongoose.connect('mongodb://localhost:27017/Todo',{
+mongoose.connect('mongodb://localhost:27017/todos',{
     useNewUrlParser:true
 })
 const connection = mongoose.connection;
@@ -19,33 +19,34 @@ connection.once('open',function(){
     console.log("MongoDB database connection established successfully");
 })
 
-todoRoutes.route('/').get(function(req,res){
-    Todo.find(function(err,todos){
-        if(err){
+todoRoutes.route('/').get(function(req, res) {
+    Todo.find(function(err, todos) {
+        if (err) {
             console.log(err);
-        }
-        else{
+        } else {
             res.json(todos);
         }
-    })
-})
-
-todoRoutes.route('/:id').get(function(req,res){
-    let id= req.params.id;
-    Todo.findById(id,function(err,todo){
+    });
+});
+todoRoutes.route('/:id').get(function(req, res) {
+    let id = req.params.id;
+    Todo.findById(id, function(err, todo) {
         res.json(todo);
-    })
-})
+    });
+});
 
-todoRoutes.route('/add').post(function(req,res){
+todoRoutes.route('/add').post(function(req, res) {
     let todo = new Todo(req.body);
     todo.save()
-    .then(todo=>{
-        res.status(200).json({'todo':'todo added successfully'});
-    })
-    .catch(err=>{
-        res.status(400).send('adding new todo failed');
-    })
+        .then(todo => {
+            res.status(200).json({
+                'todo': 'todo added successfully'});
+                console.log(todo);
+        })
+        .catch(err => {
+            res.status(400).send('adding new todo failed');
+            console.log(err);
+        });
 });
 
 todoRoutes.route('/update/:id').post(function(req,res){
